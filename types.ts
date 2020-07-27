4d010eff796b6ee2f984d1588d1d8a69f6a511df
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-07-27 09:20:56 +0700

    typescript type annotations and inference

---
+++ types.ts
@@ -0,0 +1,13 @@
+import { toNamespacedPath } from 'path';
+
+const today = new Date();
+today.getMonth();
+
+const person = {
+  age: 20,
+};
+class Color {
+  name: 'red';
+}
+const red = new Color();
+red.name;
